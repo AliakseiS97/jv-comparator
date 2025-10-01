cbb9c67e4e25bd4cb6b1177578b23565954acee7
AliakseiS97 <aliakseis97@gmail.com>
2025-10-01 11:50:15 +0200

    Rename calling metods in test

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -6,7 +6,7 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class EmployeeService {
-    public Set<Employee> getEmployByOrder(List<Employee> employees) {
+    public Set<Employee> getEmployeeByOrder(List<Employee> employees) {
         Set<Employee> set = new TreeSet<>(new Comparator<Employee>() {
             @Override
             public int compare(Employee o1, Employee o2) {
@@ -17,6 +17,9 @@ public class EmployeeService {
                 return o1.getName().compareTo(o2.getName());
             }
         });
+        if (employees == null) {
+            return set;
+        }
         set.addAll(employees);
         return set;
     }
